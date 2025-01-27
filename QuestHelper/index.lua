56ef73403250a7df631f3ae6d249e906340e4c91
mudzereli <mudz@charter.net>
2025-01-26 20:13:54 -0600

    update QuestHelper
    - add "Undo" button which unchecks previously checked step

--- QuestHelper/index.lua
+++ QuestHelper/index.lua
@@ -6,6 +6,7 @@ local inputQuestText = "paste quest text here"
 local inputQuestTextSize = Vector2.new(350, 350)
 local initSize = false
 local questSteps = {}
+local questStepsCompleted = {}
 
 local hud = ubviews.Huds.CreateHud("QuestHelper v" .. version)
 hud.ShowInBar = true
@@ -18,12 +19,31 @@ hud.OnRender.Add(function()
     if imgui.BeginTabBar("QuestHelper Tab Bar") then
         if imgui.BeginTabItem("Quest Steps") then
             
-            if questSteps[1] ~= nil then
-                if imgui.Button("Say Current Step") then
-                    game.Actions.InvokeChat(questSteps[1][1])
+            if imgui.BeginTable("ButtonTable",2) then
+                imgui.TableSetupColumn("ButtonCol1")
+                imgui.TableSetupColumn("ButtonCol2")
+                imgui.TableNextRow()
+                if questSteps[1] ~= nil then
+                    imgui.TableNextColumn()
+                    if imgui.Button("Say Current Step") then
+                        game.Actions.InvokeChat(questSteps[1][1])
+                    end
                 end
+                
+                local numCompletedSteps = #questStepsCompleted
+                if numCompletedSteps > 0 then
+                    imgui.TableNextColumn()
+                    if imgui.Button("Undo") then
+                        if numCompletedSteps > 0 then
+                            local step = questStepsCompleted[numCompletedSteps]
+                            table.insert(questSteps,step[2],{step[1],false})
+                            table.remove(questStepsCompleted,numCompletedSteps)
+                        end
+                    end
+                end
+                imgui.EndTable()
             end
-            
+
             local i = 1
             while i <= #questSteps do
                 local step = questSteps[i][1]
@@ -42,6 +62,7 @@ hud.OnRender.Add(function()
                 -- If the checkbox is checked, remove this step from the list
                 if cbxChanged and not check then
                     table.remove(questSteps, i)
+                    table.insert(questStepsCompleted,{step,i})
                 else
                     i = i + 1
                 end
